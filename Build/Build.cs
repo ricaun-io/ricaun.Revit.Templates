be7984eeb11723d85349553f5cef3f81493170c8
ricaun <ricaun@gmail.com>
2026-08-17 14:59:49 -0300

    Update build with test and beforecompile

--- Build/Build.cs
+++ Build/Build.cs
@@ -7,8 +7,9 @@ using ricaun.Nuke;
 using ricaun.Nuke.Components;
 using ricaun.Nuke.Extensions;
 
-class Build : NukeBuild, IPublishPack, ITemplateInstaller, IPrePack
+class Build : NukeBuild, IPublishPack, ITemplateInstaller, ICompileBefore, IPrePack, ITest
 {
+    string IHazCompileBefore.Name => "ricaun.Revit.Sdk*";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
 
