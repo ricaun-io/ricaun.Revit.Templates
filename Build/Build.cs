21c56df2c850b0a67ed1cba8cf5f5849858e655f
ricaun <ricaun@gmail.com>
2026-08-19 14:42:53 -0300

    Update build sign

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,11 +3,9 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack, ICompileBefore, ICompileAfter, IPrePack, ITest
+class Build : NukeBuild, IPublishPack, ICompileBefore, ICompileAfter, IPrePack, ITest, ITemplateInstaller
 {
-    bool IHazCompileBefore.SignCompile => false;
     string IHazCompileBefore.Name => "ricaun.Revit.Sdk";
-    bool IHazCompileAfter.SignCompile => false;
     string IHazCompileAfter.Name => "ricaun.Revit.Sdk.Sample";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
