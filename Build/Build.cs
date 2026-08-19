708ea1df616d247d4846d27f89827236213a9e47
ricaun <ricaun@gmail.com>
2026-08-19 14:31:55 -0300

    Version 0.12.0-alpha.11

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,58 +1,13 @@
 using Nuke.Common;
 using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Utilities.Collections;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
-using ricaun.Nuke.Extensions;
 
-class Build : NukeBuild, IPublishPack, ITemplateInstaller, ICompileBefore, ICompileAfter, IPrePack, ITest
+class Build : NukeBuild, IPublishPack, ICompileBefore, ICompileAfter, IPrePack, ITest
 {
     bool IHazCompileBefore.SignCompile => false;
     string IHazCompileBefore.Name => "ricaun.Revit.Sdk";
     bool IHazCompileAfter.SignCompile => false;
     string IHazCompileAfter.Name => "ricaun.Revit.Sdk.Sample";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
-}
-
-/// <summary>
-/// Create Template Manual Installer
-/// </summary>
-public interface ITemplateInstaller : IRelease, IHazRelease, IHazContent, ISign, INukeBuild
-{
-    Target TemplateInstaller => _ => _
-        .TriggeredBy(Compile)
-        .After(Sign)
-        .Before(Release)
-        .Executes(() =>
-        {
-            CreateTemplateInstaller(MainProject);
-        });
-
-    /// <summary>
-    /// CreateTemplateInstaller
-    /// </summary>
-    /// <param name="project"></param>
-    public void CreateTemplateInstaller(Project project)
-    {
-        if (!ContentDirectory.DirectoryExists())
-        {
-            Serilog.Log.Warning($"Skip Not found: {ContentDirectory}");
-            return;
-        }
-
-        var fileName = $"{project.Name}.Installer";
-        var ProjectDirectory = ReleaseDirectory / fileName;
-
-        Globbing.GlobFiles(ContentDirectory, "*.dll")
-                .ForEach(file => file.DeleteFile());
-
-        AbsolutePathExtensions.Copy(ContentDirectory, ProjectDirectory);
-
-        var zipFile = ReleaseDirectory / $"{fileName}.zip";
-        ZipExtension.CreateFromDirectory(ProjectDirectory, zipFile);
-
-        Serilog.Log.Information($"Create: {fileName}.zip");
-    }
 }
