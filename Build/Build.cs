1f4c83704690f113aed707091f4533c6bc9a9cb7
ricaun <ricaun@gmail.com>
2026-08-25 15:17:06 -0300

    Update build

--- Build/Build.cs
+++ Build/Build.cs
@@ -2,10 +2,55 @@ using Nuke.Common;
 using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Utilities.Collections;
+using ricaun.Nuke.Extensions;
 
 class Build : NukeBuild, IPublishPack, ICompileBefore, ICompileAfter, IPrePack, ITest, ITemplateInstaller
 {
     string IHazCompileBefore.Name => "ricaun.*.Sdk";
     string IHazCompileAfter.Name => "ricaun.*.Sdk.Sample";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
+}
+
+/// <summary>
+/// Create Template Manual Installer
+/// </summary>
+public interface ITemplateInstaller : IRelease, IHazRelease, IHazContent, ISign, INukeBuild
+{
+    Target TemplateInstaller => _ => _
+        .TriggeredBy(Compile)
+        .After(Sign)
+        .Before(Release)
+        .Executes(() =>
+        {
+            CreateTemplateInstaller(MainProject);
+        });
+
+    /// <summary>
+    /// CreateTemplateInstaller
+    /// </summary>
+    /// <param name="project"></param>
+    public void CreateTemplateInstaller(Project project)
+    {
+        if (!ContentDirectory.DirectoryExists())
+        {
+            Serilog.Log.Warning($"Skip Not found: {ContentDirectory}");
+            return;
+        }
+
+        var fileName = $"{project.Name}.Installer";
+        var ProjectDirectory = ReleaseDirectory / fileName;
+
+        Globbing.GlobFiles(ContentDirectory, "*.dll")
+                .ForEach(file => file.DeleteFile());
+
+        AbsolutePathExtensions.Copy(ContentDirectory, ProjectDirectory);
+
+        var zipFile = ReleaseDirectory / $"{fileName}.zip";
+        ZipExtension.CreateFromDirectory(ProjectDirectory, zipFile);
+
+        Serilog.Log.Information($"Create: {fileName}.zip");
+    }
 }
